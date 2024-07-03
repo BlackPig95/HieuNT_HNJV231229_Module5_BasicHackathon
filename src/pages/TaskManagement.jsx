f660076a6394ba5841ada6c953273fdd088442e5
BlackPig95 <thisisblackpig@gmail.com>
2024-07-03 12:01:38 +0700

    update chuc nang focus input

--- src/pages/TaskManagement.jsx
+++ src/pages/TaskManagement.jsx
@@ -1,9 +1,11 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import TaskList from "./TaskList";
 import { GlobalContext } from "../context/Global";
 
 function TaskManagement()
 {
+    const inputRef = useRef(null);
+
     const { taskError, handleAddTask } = useContext(GlobalContext);
     const [ taskValue, setTaskValue ] = useState("");
     const handleChangeTask = (e) =>
@@ -16,13 +18,19 @@ function TaskManagement()
         handleAddTask(e.target.children[ 0 ].value);
         setTaskValue("");
     };
+    //Khi page load lần đầu thì focus vào ô input
+    useEffect(() =>
+    {
+        inputRef.current.focus();
+
+    }, []);
     return (
         <>
             <div className="flex justify-center items-center mt-[10%] p-0 box-border">
                 <div className="border rounded-md shadow-md py-5 px-20 min-w-[70%]">
                     <h3 className="text-center font-bold text-xl py-6">Danh sách công việc</h3>
                     <form className="flex gap-4" onSubmit={ handleSubmitForm }>
-                        <input value={ taskValue } onChange={ handleChangeTask } placeholder="Nhập tên công việc" type="text" className="focus:border-blue-700 hover:shadow-md h-9 border outline-none px-4 rounded flex-1" />
+                        <input ref={ inputRef } value={ taskValue } onChange={ handleChangeTask } placeholder="Nhập tên công việc" type="text" className="focus:border-blue-700 hover:shadow-md h-9 border outline-none px-4 rounded flex-1" />
                         <button type="submit" className="h-9 rounded px-4 border bg-blue-500 hover:bg-blue-600 focus:bg-blue-700 text-white">
                             Thêm
                         </button>
